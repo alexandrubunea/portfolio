dcc2e156af877428197c15d0793edf8328581332
Alexandru Bunea <alexandrubunea03@gmail.com>
2025-09-07 16:10:56 +0300

    fix: update Hero component styles for better readability and consistency

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,15 @@
 function Hero() {
     return (
         <section className="flex justify-center min-h-screen px-6 sm:px-20 xl:px-0 overflow-hidden">
-            <div className="glass-card mt-12 sm:mt-20 w-full max-w-7xl h-[80vh] p-8 sm:p-12 lg:p-20">
-                <div className="h-full flex flex-col justify-center space-y-8">
+            <div className="glass-card bg-zinc-50/60 mt-12 sm:mt-20 w-full max-w-7xl h-[80vh] p-8 sm:p-12 lg:p-20">
+                <div className="h-full flex flex-col justify-center space-y-8 items-center md:items-start">
                     <h1 className="text-5xl jetbrains-mono-bold text-zinc-800">Alexandru Bunea</h1>
 
                     <h2 className="text-2xl text-zinc-700 poppins-light">
                         Software Developer – Web & Embedded Systems
                     </h2>
 
-                    <blockquote className="text-lg text-zinc-600 max-w-3xl leading-relaxed poppins-regular-italic">
+                    <blockquote className="text-lg text-zinc-800 max-w-3xl leading-relaxed poppins-regular-italic">
                         Bridging the gap between software and hardware to build robust, high-performance solutions.
                     </blockquote>
 
